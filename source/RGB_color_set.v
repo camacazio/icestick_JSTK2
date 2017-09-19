de6b8080b401e53593eafb6c403024a9bca0cd28
Ryan B <bowlerrs@gmail.com>
2017-09-18 19:32:01 -0700

    Add files via upload
    Basic 4 color cycle. Joystick thumbstick button cycles through available colors.

---
+++ source/RGB_color_set.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Amber Arendas
+// 
+// Create Date:		05/23/2017
+// Module Name:		LED_joystick 
+// Project Name:	Joystick_Controller
+// Target Devices:	ICEStick
+// Tool versions:	iCEcube2
+// Description: Toggles on-board LEDs depending on joystick state
+//////////////////////////////////////////////////////////////////////////////////
+
+module colorcontrol(
+    input clk,
+	input [1:0] button,
+	
+	output [27:0] RGBcolor
+    );
+	
+	// ===========================================================================
+	// 							  Parameters, Regsiters, and Wires
+	// ===========================================================================
+	reg cunt [1:0];
+	reg red [7:0];
+	reg gre [7:0];
+	reg blu [7:0];
+	
+	always @ (posedge button[0})
+		begin
+        cunt <= cunt+1;
+		end
+		
+	// Begin
+	always @ (posedge clk)
+		begin
+		// Color shit
+		if (cunt = 0)begin
+			red <= 8'b11111111;
+			gre <= 8'b00000000;
+			blu <= 8'b00000000;
+			end
+		
+		if (cunt = 1)begin
+			red <= 8'b00000000;
+			gre <= 8'b11111111;
+			blu <= 8'b00000000;
+			end
+			
+		if (cunt = 2)begin
+			red <= 8'b00000000;
+			gre <= 8'b00000000;
+			blu <= 8'b11111111;
+			end
+			
+		if (cunt = 3)begin
+			red <= 8'b11111111;
+			gre <= 8'b11111111;
+			blu <= 8'b11111111;
+			end
+		end
+		
+	assign RGBcolor = {red, gre, blu, 8'b00000000};
+		
+endmodule
